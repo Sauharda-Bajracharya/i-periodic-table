1d9e76279ceba09302d04edcee4c4fff21a32607
Sauharda Bajracharya <98646508+Sauharda-Bajracharya@users.noreply.github.com>
2023-06-28 00:19:52 +0545

    Updated main.js
    Addition to the element info.
    - Sub shell was created

--- Grade X - First Sem Project/JS/main.js
+++ Grade X - First Sem Project/JS/main.js
@@ -1,35 +1,37 @@
 $(document).ready(function() {
 
-   
-
-    function showElementInfo(elementName, elementNumber, elementMass, elementConfiguration, elementDescription) {
-
+    function showElementInfo(elementName, elementNumber, elementMass, elementSubShell, elementDescription, knowMore) {
+        $('#element-subshell').text(elementSubShell)
         $('#element-name').text(elementName);
         $('#element-number').text('Atomic Number: ' + elementNumber);
         $('#element-mass').text('Atomic Mass: ' + elementMass);
-        $('#element-configuration').text('Electronic Configuration: ' + elementConfiguration);
-        // $('#element-description').text(elementDescription);
+        $('#element-description').text(elementDescription);
+        $('#know-more').text(knowMore);
         $('#element-info').show();
     }
-
+  
     function hideElementInfo() {
-        $('#element-info').hide();
+
+      $('#element-info').hide();
     }
 
     $('.periodic-table li').click(function() {
+
+        var elementSubShell = $(this).find('shell').attr('new');
         var elementName = $(this).find('name').attr('title');
         var elementNumber = $(this).data('atomic-number');
         var elementMass = $(this).data('atomic-mass');
         var elementConfiguration = $(this).find('electronicConfiguration').text();
-        // var elementDescription = $(this).find('.info').text();
-        showElementInfo(elementName, elementNumber, elementMass, elementConfiguration);
+        var elementDescription = $(this).find('description').attr('title');
+        var knowMore = $(this).find('more').attr('about');
+        showElementInfo(elementName, elementNumber, elementMass, elementConfiguration, elementDescription, knowMore, elementSubShell);
     });
-      
-
+  
     $(document).click(function(event) {
         var target = $(event.target);
         if (!target.closest('.periodic-table li').length && !target.is('#element-info')) {
             hideElementInfo();
         }
+        });
     });
-});
+  
